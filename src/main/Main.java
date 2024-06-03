c26ed48c593664533e1dc937626003dfb10190f6
WillOtterbein <willjordan05@icloud.com>
2024-06-02 22:53:43 -0700

    Semantic changes to the Main class, made the objects and methods static as they should be accessed via the main class in my opinion. EXCEPT for the WINDOW and CURRENT_PANEL 'fields,' which I want to be only modifiable via some methods I made, but they are modifiable via static methods so those items are private static which feels weird but I'm going for it.

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,57 +16,58 @@ import javax.swing.JPanel;
  * @version 2024-1
  */
 public class Main {
-    // TILE SIZE
-    static final int og_tile_size = 16;    // 16 x 16, pixels
-    static final int size_mod = 4;
-    static int tileSize = size_mod * og_tile_size;
+    // TILE SIZE (accessible elsewhere)
+    public static final int og_tile_size = 16;    // 16 x 16, pixels
+    public static final int size_mod = 4;
+    public static int tileSize = size_mod * og_tile_size;
     
-    // WINDOW SIZE
-    static final int max_screen_col = 16;
-    static final int max_screen_row = 12;
-    static int screenWidth = tileSize * max_screen_col;
-    static int screenHeight = tileSize * max_screen_row;
+    // WINDOW SIZE (accessible elsewhere)
+    public static final int max_screen_col = 16;
+    public static final int max_screen_row = 12;
+    public static int screenWidth = tileSize * max_screen_col;
+    public static int screenHeight = tileSize * max_screen_row;
     
-    private JFrame window;
-    private JPanel state;
+    // WINDOW RELATED OBJECTS 
+    // (the actual frame of the game and the current panel pointer)
+    private static JFrame WINDOW = null;
+    private static JPanel CURRENT_PANEL = null;
     
-    // GAME STATES
-    private JPanel main_menu = new MainMenu();            // instance of MainMenu state (JPanel)
-    private JPanel game = new Game();                     // Instance of Game state (JPanel)
-    
-	public static void main(String[] args) {
-	    Main m = new Main();                              // Instance of main
-	    
-		m.window = new JFrame();
-		m.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		m.window.setResizable(false);
-		// test
-		m.window.setTitle("KeyFinder");
-		
-		m.window.getContentPane().add(m.state);           // Add the default state of the game (Main Menu)
-		m.window.pack();                                  // causes window size to fit to the game panel
-		
-		m.window.setLocationRelativeTo(null);
-		m.window.setVisible(true);
+    // KEYFINDER GAME RELATED PANELS  (basically states)
+    private static JPanel MAIN_MENU = new MainMenu();            // instance of MainMenu state (JPanel)
+    private static JPanel GAME = new Game();                     // Instance of GAME state (JPanel)
+	
+	/**
+	 * setPanelToGAME:         changes CURRENT_PANEL to GAME.
+	 */
+	public static void setPanelToGAME() {
+	    CURRENT_PANEL = GAME;
 	}
 	
 	/**
-	 * no-arg constructor for Main
-	 * 
-	 * sets the default state to MainMenu
+	 * setPanelToMainMenu:     changes CURRENT_PANEL to MAIN_MENU
 	 */
-	Main() {
-	    state = main_menu;
+	public static void setPanelToMainMenu() {
+	    CURRENT_PANEL = MAIN_MENU;
 	}
-	
-	/**
-	 * changeState:    Change the current state of the game
-	 * 
-	 * Change from MainMenu to Game, current states
-	 * 
-	 * @param state
-	 */
-	public void changeState(JPanel newPanel) {
-	    window.getContentPane().add(newPanel);              // Switch to the new state
-	}
+    
+    /**
+     * main:        program entry
+     * 
+     * @param args
+     */
+    public static void main(String[] args) { 
+        setPanelToMainMenu();                                // Default start
+        
+        WINDOW = new JFrame();
+        WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        WINDOW.setResizable(false);
+        // test
+        WINDOW.setTitle("KeyFinder");
+        
+        WINDOW.getContentPane().add(CURRENT_PANEL);           // Add the default state of the GAME (Main Menu)
+        WINDOW.pack();                                        // causes WINDOW size to fit to the GAME panel
+        
+        WINDOW.setLocationRelativeTo(null);
+        WINDOW.setVisible(true);
+    }
 }
